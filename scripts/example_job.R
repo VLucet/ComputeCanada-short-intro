b52d7bcb5710a24567afaeda5751b3e85edc5e14
Valentin <valentin.lucet@gmail.com>
2021-08-01 16:34:18 -0400

    simplify script

--- scripts/example_job.R
+++ scripts/example_job.R
@@ -1,10 +1,10 @@
 
 # This example job is a simple R script that does a few useless things 
 # just to demonstrate the principles behind kjob submissions
-n <- sample(20:30, 1)
+n <- 10
 Sys.sleep(n)
 
 print(mtcars)
 
-n <- sample(20:30, 1)
+n <- 15
 Sys.sleep(n)
